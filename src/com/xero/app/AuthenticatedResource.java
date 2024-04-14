1468761e953001ebde3da54c1941e52c64ea5ee6
start2code-app <53997286+start2code-app@users.noreply.github.com>
2024-04-14 14:30:21 +0100

    Update AuthenticatedResource.java

--- src/com/xero/app/AuthenticatedResource.java
+++ src/com/xero/app/AuthenticatedResource.java
@@ -296,6 +296,10 @@ public class AuthenticatedResource extends HttpServlet {
         String savedRefreshToken = store.get(request, "refresh_token");
         String xeroTenantId = store.get(request, "xero_tenant_id");
 
+        System.out.println(savedRefreshToken)
+        System.out.println(xeroTenantId)
+            
+
         // Check expiration of token and refresh if necessary
         // This should be done prior to each API call to ensure your accessToken is
         // valid
